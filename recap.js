ef649f3326f2f1a4a0b3caabbaece8b4ba63db81
bethcreate <kamaubetth@gmail.com>
2021-03-23 16:25:44 +0300

    grades

--- recap.js
+++ recap.js
@@ -1,16 +1,18 @@
-const marks=90;
+var marks=40;
 if(marks>=80){
     console.log("I got an A"); 
 
-} else {(marks>=79)
+} else if(marks>=79)
     console.log("I got a B"); 
 
-}if(marks>=69){
+else if(marks>=69){
     console.log("I got a C"); 
 }
-if(marks>=59){
+else if(marks>=59){
     console.log("I got a D"); 
-    
-}if(marks>=49){
+
+}else if(marks>=49){
     console.log("I got an E");
+}else{
+    console.log("I got F");
 }
