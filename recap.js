44e846e4ef17af83cee841ede460ac14c39bd6ee
bethcreate <kamaubetth@gmail.com>
2021-03-23 15:02:06 +0300

    first commit

---
+++ recap.js
@@ -0,0 +1,16 @@
+const marks=90;
+if(marks>=80){
+    console.log("I got an A"); 
+
+} else {(marks>=79)
+    console.log("I got a B"); 
+
+}if(marks>=69){
+    console.log("I got a C"); 
+}
+if(marks>=59){
+    console.log("I got a D"); 
+    
+}if(marks>=49){
+    console.log("I got an E");
+}
